8aaf6009944d9b6ad500cfde1869efcf45b1d425
unknown <vleague2@gmail.com>
2018-04-15 15:12:08 -0400

    adjusting weather div, added other content rows

--- assets/app.js
+++ assets/app.js
@@ -9,17 +9,12 @@ $("#user").on("click", function() {
     $(".search-content").css('display', 'none');
 
     // Starting the API info for the Weather API
-    // Defining the start of the URL
-    var openWeatherURL = "http://api.openweathermap.org/data/2.5/forecast?";
 
     // query parameter
     var openWeatherparam = "&q="+userCity
 
-    // API key parameter
-    var openWeatherApik ="&APPID=9155ad4470b3c881f026f9305727169c";
-
     // putting it all together in a queryURL
-    var queryURL2 = openWeatherURL + openWeatherparam + openWeatherApik;
+    var queryURL2 = "http://api.openweathermap.org/data/2.5/forecast?" + openWeatherparam + "&APPID=9155ad4470b3c881f026f9305727169c";
     
     // AJAX call for Open Weather API
     $.ajax({
@@ -43,12 +38,27 @@ $("#user").on("click", function() {
         // Pull and round the current temperature converted to F, store in a variable
         var temperature = Math.round((response.list["0"].main.temp - 273.15) * 1.8 + 32);
         
-        // Setting up the dynamically-generated html div for the weather!
+        // Setting up the dynamically-generated divs for the page!
 
-        // row to house content
+        var container = $("<div class='container'>");
+
+        $("main").append(container);
+
+        // rows to house content
         var weatherRow = $("<div class='row'>");
-            // column to limit width
-            var weatherCol = $("<div class='col s6 offset-s3'>");
+
+        var buttonRow = $("<div class='row'>");
+
+        var mapRow = $("<div class='row'>")
+
+        // append all rows to the container
+        container.append(weatherRow).append(buttonRow).append(mapRow);
+
+
+            // WEATHER SECTION
+
+            // column for Weather to limit width
+            var weatherCol = $("<div class='col s10 offset-s1'>");
 
             // append!
             weatherRow.append(weatherCol);
@@ -60,7 +70,7 @@ $("#user").on("click", function() {
                 weatherCol.append(newWeatherDiv);
 
                     // Div for the image that attaches to the card
-                    var newWeatherImage = $("<div class='card-image'>");
+                    var newWeatherImage = $("<div class='card-image' style='background-size:cover'>");
 
                     // Append the div for the card image to the card horizontal div
                     newWeatherDiv.append(newWeatherImage);
@@ -75,7 +85,7 @@ $("#user").on("click", function() {
                             var weatherConditionImage = "assets/images/rain.png";
                             
                             // add in an image based on the weather. NEED TO DO IMAGES IN AN IF/ELSE
-                            newWeatherImage.append($("<img src=" + weatherConditionImage + ">"));
+                            newWeatherImage.append($("<img height='200px' src=" + weatherConditionImage + ">"));
                         }
 
                         else {
@@ -91,32 +101,33 @@ $("#user").on("click", function() {
                     // Append the card stacked div to the card horizontal div
                     newWeatherDiv.append(newWeatherCard);
 
-                    // make a div that identifies the card content
-                    var newWeatherContent = $("<div class='card-content'>");
+                        // make a div that identifies the card content
+                        var newWeatherContent = $("<div class='card-content'>");
 
-                    // Append the card content div to the card horizontal div
-                    newWeatherCard.append(newWeatherContent);
+                        // Append the card content div to the card horizontal div
+                        newWeatherCard.append(newWeatherContent);
 
-                        // Adding in the text for the card
-                        newWeatherContent.append("<p style='font-size: 20px' class='center-align'>Current weather conditions for " + userCity + "</p><br>")
+                            // Adding in the text for the card
+                            newWeatherContent.append("<p style='font-size: 20px' class='center-align'>Weather conditions for " + userCity + "</p><br>")
 
-                        // Append a P tag that will hold the weather info
-                        newWeatherContent.append("<p class='center-align'>" + nowWeatherDescription + " | " + humidity + "% humidity | " + temperature + " &#176 F</p><br>")
+                            // Append a P tag that will hold the weather info
+                            newWeatherContent.append("<p class='center-align'>Next 3 hours:<br>" + nowWeatherDescription + " | " + humidity + "% humidity | " + temperature + " &#176 F</p><br>")
 
-                        // Append a suggestion
-                        if (nowWeather == "Clouds") {
-                            newWeatherContent.append("<p class='center-align'>Today would be a great day to go hiking or mountain biking!</p>");
-                        }
+                            // Append a suggestion
+                            if (nowWeather == "Clouds") {
+                                newWeatherContent.append("<p class='center-align'>Today would be a great day for hiking or biking!</p>");
+                            }
 
-                        else if (nowWeather == "Rain") {
-                            newWeatherContent.append("<p class='center-align'>It might be a good idea to stay home or visit a museum, but if you want to brave the weather...</p>");
-                        }
+                            else if (nowWeather == "Rain") {
+                                newWeatherContent.append("<p class='center-align'>It might be a good idea to stay home or go to the gym.</p>");
+                            }
 
-                        else {
-                            newWeatherContent.append("<p class='center-align'>It's a beautiful day to visit a park or go camping!</p>");
-                        }
+                            else {
+                                newWeatherContent.append("<p class='center-align'>It's a beautiful day to visit a park or go camping!</p>");
+                            }
 
-        $("main").append(weatherRow);
+
+
     });
 })
 
